0001ca4c2e50f4692e99b8ee149043161089c020
TImessi <toto22767@gmail.com>
2025-02-07 21:50:03 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+void reverse(char *str);
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    char text[50];
+    printf("Enter str1: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = 0; 
+    
+    reverse(text);
+    printf("MAIN : %s\n", text);
+    return 0;
+}
+
+void reverse(char *str) {
+    int text = strlen(str);
+    for (int i = 0; i < text / 2; i++) {
+        char temp = str[i];
+        str[i] = str[text - i - 1];
+        str[text - i - 1] = temp;
+    }
+    printf("FUNC : %s\n", str);
+}
